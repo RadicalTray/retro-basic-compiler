e50e6d39c60d7906db9ccf95dc7a21a7b47fe5f3
radicaltray <radicaltray@gmail.com>
2026-04-05 01:32:18 +0700

    impl scanner

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,29 @@
 const std = @import("std");
+const scanner = @import("./scanner.zig");
+
+const example =
+\\10 A = 1
+\\20 S = 0
+\\30 IF 10 < A 70
+\\40 S = S + A
+\\50 A = A + 1
+\\60 GOTO 30
+\\70 PRINT S
+\\80 STOP
+;
 
 pub fn main() !void {
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    const gpa = std.heap.smp_allocator;
+
+    var token_arena_allocator: std.heap.ArenaAllocator = .init(gpa);
+    defer token_arena_allocator.deinit();
+    const token_arena = token_arena_allocator.allocator();
+    const tokens = try scanner.scan(token_arena, example);
+    for (tokens) |t| {
+        if (t.symbol == .newline) {
+            std.debug.print("newline\n", .{});
+        } else {
+            std.debug.print("{s} ({s})\n", .{@tagName(t.symbol), t.lexeme});
+        }
+    }
 }
---
+++ src/scanner.zig
@@ -0,0 +1,108 @@
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+const eql = std.mem.eql;
+
+pub const Token = struct {
+    symbol: Symbol,
+    lexeme: []const u8,
+    line: u32,
+
+    pub fn init(symbol: Symbol, lexeme: []const u8, line: u32) Token {
+        return .{
+            .symbol = symbol,
+            .lexeme = lexeme,
+            .line = line,
+        };
+    }
+};
+
+pub const Symbol = union(enum) {
+    const Tag = std.meta.Tag(@This());
+
+    // 1 char
+    equal,
+    less,
+    greater,
+    plus,
+    minus,
+    newline, // statement separator
+
+    // 2 char
+    less_or_equal,
+    greater_or_equal,
+
+    // multiple char
+    // keywords
+    print,
+    goto,
+    stop,
+    @"if",
+
+    number: u64, // constants and line numbers are positive integers
+    identifier: u8, // one [A-Z] only. warns/errors out multiple char identifier if more than 1 letter
+};
+
+pub fn scan(arena: Allocator, source: []const u8) ![]Token {
+    var tokens: std.ArrayList(Token) = try .initCapacity(arena, 128);
+    errdefer tokens.deinit(arena);
+
+    var start: u32 = 0;
+    var current: u32 = 0;
+    var line: u32 = 1;
+
+    while (current < source.len) : (start = current) {
+        const c = source[current];
+        current += 1;
+
+        const symbol: Symbol = switch (c) {
+            '=' => .equal,
+            '+' => .plus,
+            '-' => .minus,
+            '\n' => blk: {
+                line += 1;
+                break :blk .newline;
+            },
+            '<' => blk: {
+                if (peek(source, current) == '=') {
+                    current += 1;
+                    break :blk .less_or_equal;
+                } else {
+                    break :blk .less;
+                }
+            },
+            '>' => blk: {
+                if (peek(source, current) == '=') {
+                    current += 1;
+                    break :blk .greater_or_equal;
+                } else {
+                    break :blk .greater;
+                }
+            },
+            '0'...'9' => blk: {
+                while ('0' <= peek(source, current) and peek(source, current) <= '9') current += 1;
+                break :blk .{ .number = try std.fmt.parseInt(u32, source[start..current], 10) }; // TODO: handle parseInt error
+            },
+            'A'...'Z' => blk: {
+                while ('A' <= peek(source, current) and peek(source, current) <= 'Z') current += 1;
+                const lexeme = source[start..current];
+
+                if (lexeme.len == 1) break :blk .{ .identifier = c };
+                if (eql(u8, lexeme, "PRINT")) break :blk .print;
+                if (eql(u8, lexeme, "GOTO")) break :blk .goto;
+                if (eql(u8, lexeme, "STOP")) break :blk .stop;
+                if (eql(u8, lexeme, "IF")) break :blk .@"if";
+                return error.InvalidIdentifier;
+            },
+            ' ' => continue,
+            else => return error.UnexpectedCharacter,
+        };
+        try tokens.append(arena, .init(symbol, source[start..current], line));
+    }
+
+    return try tokens.toOwnedSlice(arena);
+}
+
+fn peek(arr: []const u8, idx: u32) u8 {
+    if (idx >= arr.len) return 0;
+    return arr[idx];
+}
